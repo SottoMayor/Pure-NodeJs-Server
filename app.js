2daca0d31d420c702667951069a829bf526f56ed
David Sotto Mayor <ddsmmf@gmail.com>
2021-09-13 17:07:55 -0400

    Working with requests and responses

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // Working with requests
+    console.log(req.url, req.method, req.headers);
+
+    // Working with responses
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>NodeJs Page</title></head>')
+    res.write('<body><h1>NodeJs says: "Hey Buddy!"</h1></body>')
+    res.write('</html>')
+    res.end();
 })
 
 // To starts up the server, write in the terminal 'node app.js' 
